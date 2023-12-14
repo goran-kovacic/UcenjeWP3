f5b13fe35ae973f0c7e8ed7133f00783716170bd
goran-kovacic <132432118+goran-kovacic@users.noreply.github.com>
2023-12-14 20:58:11 +0100

    unos podataka u tablicu smjer

---
+++ insert.sql
@@ -0,0 +1,11 @@
+
+use edunovawp3;
+
+select * from smjerovi;
+
+insert into smjerovi(naziv, trajanje, cijena, vaucer) 
+values ('web programiranje', 225, 1859.45, 1 );
+
+insert into smjerovi values ('web dizajn', null, null, null);
+
+insert into smjerovi (naziv) values ('đšđš ĐŠĐŠĐČ');
