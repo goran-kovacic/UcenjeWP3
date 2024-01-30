2b8911d860395a844da8087bb246d1646760bdb7
goran-kovacic <kovacic.go@gmail.com>
2024-01-30 11:10:25 +0100

    prvi

---
+++ CSHARP/UcenjeWP/ConsoleApp/E01UlazIzlaz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    
+    internal class E01UlazIzlaz
+    {
+        public static void Izvedi()
+        {
+            string Ime;
+            Console.WriteLine("unesi ime: ");
+            Ime = Console.ReadLine();
+            Console.WriteLine(Ime);
+        }
+
+    }
+}
---
+++ CSHARP/UcenjeWP/ConsoleApp/Program.cs
@@ -0,0 +1,3 @@
+﻿using ConsoleApp;
+
+E01UlazIzlaz.Izvedi();
